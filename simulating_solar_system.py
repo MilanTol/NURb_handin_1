03b46c8189255f5da3ab35f1ebf6034b6b6acb4b
Milan Tol <milantol35@gmail.com>
2026-06-06 15:16:24 +0200

    created get_initial_conditions

--- simulating_solar_system.py
+++ simulating_solar_system.py
@@ -43,11 +43,11 @@ marsvelocity = mars[1]
 # calculate the x position in AU
 print(marsposition.x.to_value(u.AU))
 
-# calculate the v_x velocity in AU/day
-print(marsvelocity.x.to_value(u.AU / u.d))
+# calculate the v_x velocity in AU/yr
+print(marsvelocity.x.to_value(u.AU / u.yr))
 
 
-def get_initial_conditions(time: Time) -> tuple[np.ndarray, np.ndarray]:
+def get_initial_conditions(time:Time, bodies:dict) -> tuple[np.ndarray, np.ndarray]:
     """
     Generate initial conditions for the Solar System using get_body_barycentric_posvel.
 
@@ -57,6 +57,8 @@ def get_initial_conditions(time: Time) -> tuple[np.ndarray, np.ndarray]:
     ----------
     time : astropy.time.Time
         Time at which the initial conditions are evaluated.
+    bodies : dictionary
+        Dictionary containing names of bodies as keys
 
     Returns
     -------
@@ -65,10 +67,35 @@ def get_initial_conditions(time: Time) -> tuple[np.ndarray, np.ndarray]:
     velocities : ndarray
         Initial velocities, shape (N_bodies, 3).
     """
-    # TODO: implement this function using the example above
-    return np.zeros((len(bodies_with_masses), 3)), np.zeros(
-        (len(bodies_with_masses), 3)
-    )
+    # instantiate lists for positions and velocities
+    positions = []
+    velocities = []
+    
+    # loop over all bodies
+    for body in bodies.keys():
+        # load in position and velocity of body
+        with solar_system_ephemeris.set("jpl"):
+            body_posvel = get_body_barycentric_posvel(body, t)
+        body_pos = body_posvel[0]
+        body_vel = body_posvel[1]
+        # convert position units to AU
+        x = body_pos.x.to_value(u.AU)
+        y = body_pos.y.to_value(u.AU)
+        z = body_pos.z.to_value(u.AU)
+        body_pos = np.array([x, y, z])
+        # convert velocity units to AU/yr
+        v_x = body_vel.x.to_value(u.AU / u.yr)
+        v_y = body_vel.y.to_value(u.AU / u.yr)
+        v_z = body_vel.z.to_value(u.AU / u.yr)
+        body_vel = np.array([v_x, v_y, v_z])
+        # store position and velocity in respective lists
+        positions.append(body_pos)
+        velocities.append(body_vel)
+        
+    # convert position and velocitiy lists to arrays
+    positions = np.array(positions)
+    velocities = np.array(velocities)
+    return positions, velocities
 
 
 def compute_accelerations(
