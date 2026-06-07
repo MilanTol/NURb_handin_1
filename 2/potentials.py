9c28815cab6058299e44eb62153132bd3dbae30e
Milan Tol <milantol35@gmail.com>
2026-06-07 21:56:50 +0200

    .

--- 2/potentials.py
+++ 2/potentials.py
@@ -23,44 +23,51 @@ class Octree_Node:
 
     Parameters
     ----------
-    center_position : ndarray
-        Geometrical center of the node, shape (3,)
+    index : ndarray
+        index of the node, shape (3,).
+        From the index of the node the center_position is calculated.
+        the center position can be retrieved using the get_center_position method.
     depth : int
         Depth of the node. The root has depth 0
     index : tuple
         Integer index of the node at its depth
     CoM : ndarray
         Centre-of-mass position, shape (3,)
-    particles : ndarray
-        Particle indices inside the node
-        This is only needed for leaf nodes / bonus question
     children : ndarray or None
         Child nodes, shape (2, 2, 2), or None for leaf nodes
+    particles : ndarray or None
+        Particle indices inside the node
+        This is only needed for leaf nodes 
     """
 
     def __init__(
         self,
-        center_position,
-        depth,
-        index,
-        CoM,
-        particles,
+        index:np.ndarray,
+        depth:int,
+        CoM:np.ndarray,
         children=None,
+        particles=None,
     ): # note: the fewer of these you keep, the better! these are just examples
-        self.center_position = center_position
+        self.particles = particles
         self.depth = depth
         self.index = index
         self.CoM = CoM
-        self.particles = particles
         self.children = children
 
         if particles is None:
             self.mass = np.float32(0.0)
         else:
             self.mass = np.float32(len(particles)) * mp
+            
+    def get_center_position(self, box_dimensions:np.ndarray):
+        square_dimensions = (box_dimensions / (self.depth+1))
+        grid_point =  self.index*square_dimensions
+        offset = 0.5*square_dimensions
+        return grid_point + offset
 
 
 def build_octree(
+    particles,
     particle_positions,
     center_position,
     index,
@@ -73,6 +80,8 @@ def build_octree(
 
     Parameters
     ----------
+    particles : ndarray
+        Contains the indices of the particles inside the current node, shape (N_node,)
     particle_positions : ndarray
         Particle positions inside the current node, shape (N_node, 3)
     center_position : ndarray
@@ -95,33 +104,78 @@ def build_octree(
     # If node contains no particles
     # mass should be zero, CoM can be set equal to center_position
     # and children should be None
-
+    if len(particles) == 0:
+        return Octree_Node(
+            particles=None,
+            depth=depth,
+            index=index,
+            CoM=center_position,
+            children=None,
+        )
+        
     # if this is a leaf node, return the node without children
+    # compute the center_of_mass, since every particle has the same 
+    # mass, we can simply take the average particle position:
+    CoM = np.sum(particle_positions, axis=0)/len(particle_positions)
+    
+    # if max depth is reached return the node without creating children,
+    # set particles equal to particle_positions.
+    if depth == max_depth:
+        return Octree_Node(
+            particles=particles,
+            depth=depth,
+            index=index,
+            CoM=CoM, # should calculate center of mass
+            children=None,
+        )
+    # else: continue building the tree by recursively calling build on children
 
-    # compute depth, index, mass, CoM, and children
-    # For the bonus question, store the particle indices
+    # instantiate an empty array in which we will store the children
+    children = np.empty((2, 2, 2), dtype=object)
 
-    # split the node into 8 child nodes
-    # Each child has half the side length of the parent
-    #  compute the child index
-    # If the parent index is (ix, iy, iz), then the child index is
-    # (2*ix + offest_x, 2*iy + offest_y, 2*iz + offest_z)
+    # precompute the children box sizes:
+    child_box_size = 0.5*box_size
+    
+    # split the node into 8 child nodes  
+    for offset_x in [0, 1]:
+        for offset_y in [0, 1]:
+            for offset_z in [0, 1]:
+                # If the parent index is (ix, iy, iz), then the child index is
+                # (2*ix + offset_x, 2*iy + offset_y, 2*iz + offset_z)
+                offset = np.array([offset_x, offset_y, offset_z])
+                child_index = 2*index + offset
+                
+                # mask for particles in this octant:
+                # note that we can use the boolean property of the offsets (0, 1)
+                child_particles = (
+                    ((particle_positions[:,0] > center_position[0]) == offset[0]) &
+                    ((particle_positions[:,1] > center_position[1]) == offset[1]) &
+                    ((particle_positions[:,2] > center_position[2]) == offset[2])
+                )
+                child_particle_positions = particle_positions[child_particles]
+                
+                # child center is half a child box size separated from parent center
+                child_center_position = center_position + 0.5*(2*offset - 1)*child_box_size
+                
+                # recursively call build on the children
+                children[offset_x, offset_y, offset_z] = build_octree(
+                    particles = child_particles,
+                    particle_positions = child_particle_positions,
+                    center_position = child_center_position,
+                    index = child_index,
+                    box_size = child_box_size,
+                    depth = depth+1,
+                    max_depth = max_depth,
+                )
+    
+    return Octree_Node(
+        particles=particles,
+        index=index,
+        depth=depth,
+        CoM=CoM,
+        children=children,
+    )
 
-    #  select particles that lie inside this child node
-
-    # recursively call build on this child
-
-    return None
-
-
-# Octree_Node(
-#     center_position=center_position,
-#     depth=depth,
-#     index=index,
-#     CoM=CoM,
-#     particles=None,
-#     children=children,
-# )
 
 
 def get_node_at_level(
